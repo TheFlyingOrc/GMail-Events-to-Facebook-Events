7b3a5b0a75df5a1e57434912152cb8edd79badc1
Stephen Herr <sherr@redhat.com>
2011-05-09 17:25:47 -0400

    added a simple button and dialog

--- src/com/example/gmail2facebook/GMail2Facebook.java
+++ src/com/example/gmail2facebook/GMail2Facebook.java
@@ -1,13 +1,47 @@
 package com.example.gmail2facebook;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class GMail2Facebook extends Activity {
+	static final int DIALOG_EXPORT_ID = 0;
+	
+	private OnClickListener exportListener = new OnClickListener() {
+    	public void onClick(View v) {
+    		showDialog(DIALOG_EXPORT_ID);
+    	}
+    };
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        Button exportButton = (Button)findViewById(R.id.button);
+        exportButton.setOnClickListener(exportListener);
     }
+    
+    /** Called anytime Android receives a call to showDialog(int). */
+    protected Dialog onCreateDialog(int id) {
+    	Dialog dialog;
+    	switch(id) {
+    	case DIALOG_EXPORT_ID:
+    		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    		builder.setMessage("Event has been exported to Facebook!");
+    		dialog = builder.create();
+    		break;
+    	default:
+    		dialog = null;
+    	}
+    	return dialog;
+    }
+    
 }
